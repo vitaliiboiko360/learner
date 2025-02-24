92b963b5d7a4e1ac146057281fb1b067b12053f1
vitalii boiko <vukelan@gmail.com>
2025-02-24 23:11:04 +0200

    add home styles

--- src/home/home_entry.tsx
+++ src/home/home_entry.tsx
@@ -1,19 +1,41 @@
 import React from 'react';
 
 import css from './home.module.scss';
-import { makeUrlToArtwork, makeUrlToResource } from '../etc/util.ts'
+import { makeUrlToArtwork, makeUrlToResource } from '../etc/util.ts';
 import { Link } from 'react-router-dom';
 
 export default function HomeEntry({ element, height }) {
   const href = makeUrlToResource(element.resource);
   const { title, artwork } = element;
 
-  return (<div style={{height}} className={css.item}>
-    <Link to={href} className={css.link}>
-      <h6 className={css.title}>{title}</h6>
-      <span className={css.image}>
-        <img src={makeUrlToArtwork(artwork)} />
-      </span>
-    </Link>
-  </div>);
+  const gradients = [
+    css.gradient1,
+    css.gradient2,
+    css.gradient3,
+    css.gradient4,
+  ];
+
+  const getRandGradient = () => {
+    return gradients[Math.floor(Math.random() * gradients.length)];
+  };
+
+  const angles = [css.angle1, css.angle2, css.angle3, css.angle4];
+
+  return (
+    <div style={{ height }} className={css.item}>
+      <Link to={href} className={[css.link].join(' ')}>
+        <div
+          className={[
+            angles[Math.floor(Math.random() * angles.length)],
+            css.glowingDiv,
+          ].join(' ')}
+        >
+          <h6 className={css.title}>{title}</h6>
+          <span className={css.image}>
+            <img src={makeUrlToArtwork(artwork)} />
+          </span>
+        </div>
+      </Link>
+    </div>
+  );
 }
