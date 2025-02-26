4313acd6fe17dcc7b210bf3fd45709ddf3337cff
vitalii boiko <vukelan@gmail.com>
2025-02-26 18:16:40 +0200

    rename button

--- src/various_controls/back_home_button.tsx
+++ src/various_controls/back_home_button.tsx
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom';
 
 import FirstPageOutlined from '@mui/icons-material/FirstPageOutlined';
 import clsx from 'clsx';
-import css from './various_controls.module.scss'
+import css from './various_controls.module.scss';
 
 export default function BackHomeButton() {
   return (
     <Link className={clsx(css.backHome, css.sm)} to="/">
       <span>
         <FirstPageOutlined />
-        <span>Back Home</span>
+        <span>Home</span>
       </span>
     </Link>
   );
